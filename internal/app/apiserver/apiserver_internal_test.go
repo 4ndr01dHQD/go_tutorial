ecd5bdc48ff100fb64d5d1c0bb8c1acfd62f3582
a.cheremisin <a.cheremisin@dostaevsky.ru>
2024-11-09 20:21:53 +0300

    init commit

--- internal/app/apiserver/apiserver_internal_test.go
+++ internal/app/apiserver/apiserver_internal_test.go
@@ -13,5 +13,6 @@ func TestAPIServer_HandleHello(t *testing.T) {
 	req, _ := http.NewRequest(http.MethodGet, "/hello", nil)
 	s.handleHello().ServeHTTP(rec, req)
 
+	assert.Equal(t, rec.Code, http.StatusOK)
 	assert.Equal(t, rec.Body.String(), "Hello")
 }
